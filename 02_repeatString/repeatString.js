3b611a726a14e9c8483f751b7a2c7c189df79b68
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-09-10 16:01:31 +0000

    Pass 1 Test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    const string = [];
+    i = 0;
 
+    while (i < times) {     
+        if (i < times) {
+            string.push(word);
+        } else {
+            break;
+        }
+
+        i++;
+    };
+
+    return string.join('');
 };
 
 // Do not edit below this line
