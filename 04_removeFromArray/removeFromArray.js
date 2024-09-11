92aef0cfca13cf5ec4d6af63476397585d56f6d9
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-09-11 20:16:55 +0000

    Pass 1 Test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
 const removeFromArray = function() {
+    let startingArray = arguments[0];
+
+    for (let i = 1; i < arguments.length; i++) {
+        if (startingArray.includes(arguments[i])) {
+            startingArray.splice((startingArray.indexOf(arguments[i])), 1);
+        } else {
+            break;
+        };
+    };
+    return startingArray;
 };
 
 // Do not edit below this line
