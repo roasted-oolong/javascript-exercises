cce8cc7e435b463b28cf3d2b53a293dc2713d136
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-09-16 19:27:55 +0000

    Pass 1 Test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+    const numbers = [];
+    let total = 0;
+    let i = firstInt;
 
+    while (i >= firstInt && i <= secondInt || i >= secondInt && i <= firstInt) {
+        numbers.push(i)
+        i++
+    }
+
+    for (let i = 0;  i < numbers.length; i++) {
+        total += numbers[i]
+    }
+    return total
 };
 
 // Do not edit below this line
