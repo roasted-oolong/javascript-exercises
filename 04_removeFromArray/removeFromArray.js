5d2502303e58643526069e59dc6a7e1d778d585d
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-09-16 18:51:41 +0000

    Pass 6 Tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
 const removeFromArray = function(startingArray, ...afterArray) {
-     for (let i = 0; i < startingArray.length - 1; i++) {
+     for (let i = 0; i < startingArray.length; i++) {
         if (afterArray.includes(startingArray[i])) {
             startingArray.splice(i, 1);
             i--
