436f01fa34ba7a61ee1df7f6b3264a58d42a2360
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-09-16 21:29:08 +0000

    Pass 1 Test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  let Celcius = (Fahrenheit -32) * 5/9
+  return (Math.round(Celcius * 10)/10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Celsius) {
+  let Fahrenheit = (Celsius * 1.8) + 32
+  return (Math.round(Fahrenheit * 10)/10);
 };
 
 // Do not edit below this line
