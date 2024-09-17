036fb48687eba02dd80127f32a9b01d55f61b027
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-09-17 16:51:28 +0000

    Pass 1 Test

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let sum = 0;
+    let i = 0;
+    const fibonacciArray = [0, 1]
+    
+    if (typeof num === "string") {
+        let num = Number(num);
+    }
+    if (Math.sign(num) === -1) {
+        return "OOPS";
+    }
 
+    if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    } else {
+        for (i = 2; i <= num; i++) {
+            let sum = fibonacciArray[i-1] + fibonacciArray[i-2]
+
+            if (i === num) {
+                return sum;
+            } else {
+                fibonacciArray.push(sum);
+            }
+        }
+    }
 };
 
 // Do not edit below this line
