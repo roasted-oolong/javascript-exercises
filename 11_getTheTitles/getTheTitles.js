b05efb4033baf395f2c1b45548f7e998dd7b45bc
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-09-17 19:53:41 +0000

    Complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    const bookTitles = [];
 
+    for (i = 0; i < books.length; i++) {
+        bookTitles.push(books[i].title);
+    }
+    return bookTitles;
 };
 
 // Do not edit below this line
