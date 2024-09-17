13c7b7542559e57aeba8d3732c20aceeb25f2c79
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-09-17 15:20:20 +0000

    Pass 5 Tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,16 @@ const subtract = function(intOne, intTwo) {
 	return intOne - intTwo;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+  if(array.length < 1 || array == undefined){
+    return total;
+} else if (array.length >= 1) {
+  for (var i = 0; i < numbers.length; i++) {
+    total += array[i]
+    return total;
+}
+}
 };
 
 const multiply = function() {
