2a86c24038e2f1d42c520d1bece02a58b344f047
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-10-23 16:18:51 +0000

    Further work

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,18 +1,11 @@
 const getAge = people.sort((firstPerson, secondPerson) => {
     if(!YearOfDeath) {
-        YearOfDeath = new Date().getFullYear();
+        deathYear = new Date().getFullYear();
     }
-    const ageOfFirstPerson = firstPerson.YearOfBirth - firstPerson.YearOfDeath
-    const ageOfSecondPerson = secondPerson.YearOfBirth - secondPerson.YearOfDeath
+    const ageOfFirstPerson = firstPerson.YearOfDeath || deathYear - firstPerson.YearOfBirth
+    const ageOfSecondPerson = secondPerson.YearOfDeath || deathYear - secondPerson.YearOfBirth
     return ageOfFirstPerson > ageOfSecondPerson ? 1 : -1;
-})
-
-const findTheOldest = people.map(oldest => people.index[0])
-
-{
-//sort from oldest to youngest
-//oldest should have index of -0
-};
+})[0];
 
 // Do not edit below this line
 module.exports = findTheOldest;
