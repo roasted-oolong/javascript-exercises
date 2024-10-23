cd19a7c7387a106fb415bd099fcdca86c6b01fe5
Roasted Oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-10-23 16:37:35 +0000

    Fixed issues
    Problem 1: age of first and second persons were not calculating correctly
    Solution 1: include parantheses to clarify order of operations. now, it determines whether there is a year of death or not, and then calculates age
    
    Problem 2: people sorted incorrectly
    Solution 2: if ageOfFirstPerson is greater than ageOfSecondPerson, value should return -1 to bubble the person up to the front of the list.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,20 +1,11 @@
-const getAge = people => {
+const findTheOldest = people => {
     return people.sort((firstPerson, secondPerson) => {
         const currentYear = new Date().getFullYear();
-        const ageOfFirstPerson = firstPerson.yearOfDeath || currentYear - firstPerson.yearOfBirth
-        const ageOfSecondPerson = secondPerson.yearOfDeath || currentYear - secondPerson.yearOfBirth
-        return ageOfFirstPerson > ageOfSecondPerson ? 1 : -1
+        const ageOfFirstPerson = (firstPerson.yearOfDeath || currentYear) - firstPerson.yearOfBirth
+        const ageOfSecondPerson = (secondPerson.yearOfDeath || currentYear) - secondPerson.yearOfBirth
+        return ageOfFirstPerson > ageOfSecondPerson ? -1 : 1
     })[0];
 }
-
-// people.sort((firstPerson, secondPerson) => {
-//     if(!firstPerson.YearOfDeath || !secondPerson.YearOfDeath) {
-//         deathYear = new Date().getFullYear();
-//     }
-//     const ageOfFirstPerson = firstPerson.YearOfDeath || deathYear - firstPerson.YearOfBirth
-//     const ageOfSecondPerson = secondPerson.YearOfDeath || deathYear - secondPerson.YearOfBirth
-//     return ageOfFirstPerson > ageOfSecondPerson ? 1 : -1;
-// })[0];
 
 // Do not edit below this line
 module.exports = findTheOldest;
